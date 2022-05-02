308139b61e9b43e78ba93c500e439c3de4ae1658
tanverrana <tanversobur@gmail.com>
2022-05-02 09:26:25 +0600

    required auth with name

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,19 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         });
+        app.get("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
+
+        //Post
+        app.post("/service", async (req, res) => {
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        })
     }
     finally {
 
