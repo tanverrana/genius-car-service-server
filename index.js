ea7c16a67c300db0c6eaa1a58f49948f9af4afc3
tanverrana <tanversobur@gmail.com>
2022-05-07 08:57:26 +0600

    db data load

--- index.js
+++ index.js
@@ -48,6 +48,15 @@ async function run() {
         });
 
         //order collection api
+
+        app.get("/order", async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.post("/order", async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
