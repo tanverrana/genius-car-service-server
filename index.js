c0df2b01ba531cb9436c60411cbcee254ecc3424
tanverrana <tanversobur@gmail.com>
2022-05-07 08:09:58 +0600

    create order collection api

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db("geniusCar").collection("service");
+        const orderCollection = client.db("geniusCar").collection("order");
 
         app.get("/service", async (req, res) => {
             const query = {};
@@ -44,7 +45,15 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await serviceCollection.deleteOne(query);
             res.send(result);
+        });
+
+        //order collection api
+        app.post("/order", async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
+
     }
     finally {
 
